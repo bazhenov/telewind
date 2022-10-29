7f01fca00fd6c1838bd055ddf448e3b9c61aa162
Denis Bazhenov <dotsid@gmail.com>
2022-10-30 00:33:06 +0600

    clap.rs added

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
+use clap::Parser;
 use parser::parse;
 
 mod parser;
 
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Opts {
+    /// source url to download
+    #[arg(short, long, default_value_t = String::from("http://3volna.ru/anemometer/getwind?id=1"))]
+    url: String,
+}
+
 #[tokio::main]
 async fn main() {
-    let url = "http://3volna.ru/anemometer/getwind?id=1";
-
-    let body = reqwest::get(url)
-        .await
-        .unwrap()
-        .text()
-        .await
-        .unwrap();
+    let opts = Opts::parse();
+    let body = reqwest::get(opts.url).await.unwrap().text().await.unwrap();
 
     for observation in parse(&body) {
         println!("{observation:?}")
