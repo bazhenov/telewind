04db3e295a6b75a32ca5ea924339db37497d23da
Denis Bazhenov <dotsid@gmail.com>
2022-12-14 14:08:33 +0700

    fixed error in migration scheme

--- migrations/2022-11-29-094017_user-id-unique/up.sql
+++ migrations/2022-11-29-094017_user-id-unique/up.sql
@@ -1,7 +1,6 @@
 CREATE TABLE _tmp AS SELECT * FROM subscriptions;
 DELETE FROM subscriptions;
 CREATE UNIQUE INDEX subscriptions_user_id ON subscriptions(user_id);
-ALTER TABLE 
 INSERT INTO subscriptions
   SELECT * FROM _tmp GROUP BY user_id;
 DROP TABLE _tmp;
